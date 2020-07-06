1a1d73d5678b081f4d34628117464279fd7145b5
Dawid Rynkal <dawidos14051997@gmail.com>
2020-07-06 19:56:49 +0200

    Updates

---
+++ precache-manifest.a44411e60bd9a3aae886d5b4f40c120d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b6b7c8d15803d3920b09dfa900fdcca",
+    "url": "/WeatherApp-react/index.html"
+  },
+  {
+    "revision": "31c374fb9d2382543a92",
+    "url": "/WeatherApp-react/static/css/main.b031afb5.chunk.css"
+  },
+  {
+    "revision": "d36834ddd497d19b90df",
+    "url": "/WeatherApp-react/static/js/2.2e93365d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherApp-react/static/js/2.2e93365d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31c374fb9d2382543a92",
+    "url": "/WeatherApp-react/static/js/main.43b1dc8a.chunk.js"
+  },
+  {
+    "revision": "68aa22d04aac88950c74",
+    "url": "/WeatherApp-react/static/js/runtime-main.29bd6ffc.js"
+  },
+  {
+    "revision": "f72c80ca6d94347be731e3c6cb562936",
+    "url": "/WeatherApp-react/static/media/cold-bg.f72c80ca.jpg"
+  },
+  {
+    "revision": "c7d4f24501282e50358821cb0efa8b54",
+    "url": "/WeatherApp-react/static/media/warm-bg.c7d4f245.jpg"
+  }
+]);
